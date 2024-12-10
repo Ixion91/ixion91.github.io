0d940e0d1812daa924b5c7ee5cacbacaf77cbaf3
Ixion91 <tomas.f.esquivel@gmail.com>
2024-12-10 13:18:37 -0600

    Update email.js

--- email.js
+++ email.js
@@ -1,25 +1,4 @@
-function mail(form) {
-    var name = form.name.value;
-    var comment = form.comment.value;
-    var warning = ""
-    for (i = 0; i < form.city.length; i++) {
-        if (form.city[i].checked)
-            city += " " + form.city[i].value;
-    }
-    var str = "mailto:tomas.esquivel@ice.com?subject=Pre-Qualification Reqeust&body=";
-    if (name.length > 0) {
-        str += "Hi my name is " + name + ", ";
-    } else {
-        warning += "Name is required"
-    }
-    if (comment.length > 0) {
-        str += "%0D%0A" + comment + "."
-    }
-
-    if (warning.length > 0) {
-        alert(warning)
-    } else {
-        str += "%0D%0ARegards,%0D%0A" + name;
-        document.getElementById('send').href = str;
-    }
+function submitForm(form) {
+  window.open("mailto:tomas.esquivel@ice.com?subject=Pre-Qualification Request&body=First%20Name:%20" + form.firstname.value + "%20Last%20Name:" + form.lastname.value);
+  return false; /* cancel submit or else page reloads */
 }
