06624f3bce9f69c3eea4d051f2d8d8ba3e89832c
Ixion91 <tomas.f.esquivel@gmail.com>
2024-12-10 13:37:41 -0600

    Delete email.js

--- email.js
+++
@@ -1,4 +0,0 @@
-function submitForm(form) {
-  window.open("mailto:tomas.esquivel@ice.com?subject=Pre-Qualification Request&body=First%20Name:%20" + form.firstname.value + "%20Last%20Name:" + form.lastname.value);
-  return false; /* cancel submit or else page reloads */
-}
